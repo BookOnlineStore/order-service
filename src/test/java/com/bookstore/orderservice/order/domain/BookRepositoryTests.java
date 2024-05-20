154cd383a600b34e810502810c220ee5045e46c3
thainguyencoffee <nguyennt11032004@gmail.com>
2024-05-20 15:03:42 +0700

    setup BookRepositoryTests.java

---
+++ src/test/java/com/bookstore/orderservice/order/domain/BookRepositoryTests.java
@@ -0,0 +1,44 @@
+package com.bookstore.orderservice.order.domain;
+
+import com.bookstore.orderservice.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@DataR2dbcTest
+@Import(DataConfig.class)
+@Testcontainers
+public class BookRepositoryTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres:14.10"));
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.r2dbc.url", BookRepositoryTests::r2dbcUrl);
+        registry.add("spring.r2dbc.username", postgres::getUsername);
+        registry.add("spring.r2dbc.password", postgres::getPassword);
+        registry.add("spring.flyway.url", postgres::getJdbcUrl);
+    }
+
+    private static String r2dbcUrl() {
+        return String.format("r2dbc:postgresql://%s:%s/%s",
+                postgres.getHost(), postgres.getMappedPort(5432), postgres.getDatabaseName());
+    }
+
+    @Test
+    void setup() {
+
+    }
+}
