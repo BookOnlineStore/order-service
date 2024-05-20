3ea3f3dad2e510e57286899995c0140ba91ff8ed
thainguyencoffee <nguyennt11032004@gmail.com>
2024-05-20 23:44:06 +0700

    publish OrderAcceptedMessage to binding output

--- src/main/java/com/bookstore/orderservice/order/domain/OrderService.java
+++ src/main/java/com/bookstore/orderservice/order/domain/OrderService.java
@@ -2,6 +2,10 @@ package com.bookstore.orderservice.order.domain;
 
 import com.bookstore.orderservice.book.BookClient;
 import com.bookstore.orderservice.book.BookDto;
+import com.bookstore.orderservice.order.event.OrderAcceptedMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -9,12 +13,17 @@ import reactor.core.publisher.Mono;
 @Service
 public class OrderService {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final BookClient bookClient;
+    private final StreamBridge streamBridge;
 
-    public OrderService(OrderRepository orderRepository, BookClient bookClient) {
+    public OrderService(OrderRepository orderRepository,
+                        BookClient bookClient,
+                        StreamBridge streamBridge) {
         this.orderRepository = orderRepository;
         this.bookClient = bookClient;
+        this.streamBridge = streamBridge;
     }
 
     public Flux<Order> getAllOrder(String username) {
@@ -25,8 +34,19 @@ public class OrderService {
         return bookClient.getBookByIsbn(isbn)
                 .map(book -> buildAcceptedOrder(book, quantity))
                 .defaultIfEmpty(buildRejectedOrder(isbn, quantity))
-                .flatMap(orderRepository::save);
+                .flatMap(orderRepository::save)
+                .doOnNext(this::publishOrderAcceptedEvent);
     }
+
+    private void publishOrderAcceptedEvent(Order order) {
+        if (order.status().equals(OrderStatus.REJECTED)) {
+            return;
+        }
+        var orderAcceptedMessage = new OrderAcceptedMessage(order.id());
+        var result = streamBridge.send("acceptOrder-out-0", orderAcceptedMessage);
+        log.info("Result of sending data for order with id {}: {}", order.id(), result);
+    }
+
 
     public static Order buildRejectedOrder(String isbn, int quantity) {
         return Order.of(isbn, null,
---
+++ src/main/java/com/bookstore/orderservice/order/event/OrderAcceptedMessage.java
@@ -0,0 +1,6 @@
+package com.bookstore.orderservice.order.event;
+
+public record OrderAcceptedMessage(
+        Long orderId
+) {
+}
