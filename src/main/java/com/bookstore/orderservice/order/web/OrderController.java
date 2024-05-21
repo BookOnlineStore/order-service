43e9441d220aba317d9922a964c3a3ae1aea1a40
thainguyencoffee <nguyennt11032004@gmail.com>
2024-05-22 01:51:45 +0700

    GET /orders to get own orders

--- src/main/java/com/bookstore/orderservice/order/web/OrderController.java
+++ src/main/java/com/bookstore/orderservice/order/web/OrderController.java
@@ -5,6 +5,8 @@ import com.bookstore.orderservice.order.domain.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -12,7 +14,7 @@ import reactor.core.publisher.Mono;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("orders")
 public class OrderController {
 
     private static final Logger log = LoggerFactory.getLogger(OrderController.class);
@@ -23,9 +25,9 @@ public class OrderController {
     }
 
     @GetMapping
-    public Flux<Order> getAllOrder() {
+    public Flux<Order> getAllOrder(@AuthenticationPrincipal Jwt jwt) {
         log.info("OrderController attempt retrieve orders.");
-        return orderService.getAllOrder("unknown");
+        return orderService.getAllOrder(jwt.getSubject());
     }
 
     @PostMapping
