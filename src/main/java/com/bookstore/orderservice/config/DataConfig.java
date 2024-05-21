f0bdb2bf2f8ced19798ce62af9ac34a43dec0e15
thainguyencoffee <nguyennt11032004@gmail.com>
2024-05-22 01:51:23 +0700

    audit data by user authenticated

--- src/main/java/com/bookstore/orderservice/config/DataConfig.java
+++ src/main/java/com/bookstore/orderservice/config/DataConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.ReactiveAuditorAware;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
-import reactor.core.publisher.Mono;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 
 @Configuration
 @EnableR2dbcAuditing
@@ -12,7 +14,10 @@ public class DataConfig {
 
     @Bean
     ReactiveAuditorAware<String> reactiveAuditorAware() {
-        return () -> Mono.just("unknown");
+        return () -> ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .filter(Authentication::isAuthenticated)
+                .map(Authentication::getName);
     }
 
 }
